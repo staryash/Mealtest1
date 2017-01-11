c2d7f9149bd2a21b063a141d65659721fb9f1d41
yash <yaashg@gmail.com>
2017-01-11 23:31:07 +0530

    added mealsession class

--- src/main/java/com/Mealpack/Customer.java
+++
@@ -1,7 +0,0 @@
-package com.Mealpack;
-
-/**
- * Created by yash on 11/1/17.
- */
-public class Customer {
-}
---
+++ src/main/java/com/Mealpack/MealSession.java
@@ -0,0 +1,15 @@
+package com.Mealpack;
+
+/**
+ * Created by yash on 11/1/17.
+ */
+public class Eater {
+    private long id;
+    private String firstName, lastName;
+
+    public Eater(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
