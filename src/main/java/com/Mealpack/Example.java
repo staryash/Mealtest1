27962680a67e307f997f64e780f9d1f323405476
yash <yaashg@gmail.com>
2017-01-12 21:04:26 +0530

    woohooo

--- src/main/java/com/Mealpack/Example.java
+++ src/main/java/com/Mealpack/Example.java
@@ -1,21 +1,99 @@
 package com.Mealpack;
-
+//imports from p1
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+//imports from p2
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
 
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Example {
 
-    @RequestMapping("/home")
+    static int mealid=1;
+    static long time=1484209800000L;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+
+
+    @RequestMapping(value = "/home", produces = "application/json")
     String home() {
-        return "<h1> Food Food Food </h1>";
+        return "{}";
     }
+
+
+    @RequestMapping(value = "/filluser", produces = "application/json")
+    String filluserandgetnumber(@RequestParam("userid") int user){
+
+        long timenow;
+        timenow=System.currentTimeMillis();
+        Integer a,b,c,d;
+        d=0;
+        String e;
+
+
+        if( (timenow - time)> 3600000l )//increments mealid every 12hrs and makes new row
+        {
+            mealid=mealid+1;
+            jdbcTemplate.execute("INSERT INTO mealsession(id) VALUES ("+mealid+")");
+
+        }
+
+        if(user==1) {
+            jdbcTemplate.execute("UPDATE mealsession SET user1=1 WHERE id="+mealid);
+
+        }
+
+        else if(user==2){
+            jdbcTemplate.execute("UPDATE mealsession SET user2=1 WHERE id="+mealid);
+        }
+
+        else if(user==3){
+            jdbcTemplate.execute("UPDATE mealsession SET user3=1 WHERE id="+mealid);
+        }
+
+        a = jdbcTemplate.queryForObject("SELECT user1 from mealsession WHERE id="+mealid, Integer.class); //+mealid use
+        b = jdbcTemplate.queryForObject("SELECT user2 from mealsession WHERE id="+mealid, Integer.class);
+        c = jdbcTemplate.queryForObject("SELECT user3 from mealsession WHERE id="+mealid, Integer.class);
+
+        if(a!=null)
+            d=d+a;
+
+        if(b!=null)
+            d=d+b;
+
+        if(c!=null)
+            d=d+c;
+
+        e=d.toString();
+
+        System.out.println(d);
+        time=timenow;
+
+        return "{\"eaten\":\""+e+"\"}";
+
+    }
+
+
+
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
+
+
     }
 
 }
--- src/main/java/com/Mealpack/MealSession.java
+++ src/main/java/com/Mealpack/MealSession.java
@@ -3,13 +3,48 @@ package com.Mealpack;
 /**
  * Created by yash on 11/1/17.
  */
-public class Eater {
+public class MealSession {
     private long id;
-    private String firstName, lastName;
+    private int user1, user2, user3;
 
-    public Eater(long id, String firstName, String lastName) {
+    public MealSession(long id, int user1, int user2, int user3) {
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.user1 = user1;
+        this.user2 = user2;
+        this.user3 = user3;
     }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getUser1() {
+        return user1;
+    }
+
+    public void setUser1(int user1) {
+        this.user1 = user1;
+    }
+
+    public int getUser2() {
+        return user2;
+    }
+
+    public void setUser2(int user2) {
+        this.user2 = user2;
+    }
+
+    public int getUser3() {
+        return user3;
+    }
+
+    public void setUser3(int user3) {
+        this.user3 = user3;
+    }
+}
 
